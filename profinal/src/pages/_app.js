8df61a2a2c7ae03dc60e75fefdbe61cb4b45bda2
enriqueasc <=>
2023-07-12 01:09:30 -0600

    Ejercicio single page con React

--- profinal/src/pages/_app.js
+++ profinal/src/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css';
+//import '../styles/tailwind.css';
+import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
+
+
+
